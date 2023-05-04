10d8ea234ae42d0864012838232bf77a80e99c92
damduyr <114305300+damduyr@users.noreply.github.com>
2023-05-04 11:12:26 +0000

    Task done.

--- data.js
+++ data.js
@@ -3,16 +3,16 @@ let unityData = [
       Fomat to add your details:
       
       {
-          ContributorName: 'Your Name',
+          ContributorName: 'Your name',
           ContributorURL: "Your Github Account Link",
           ProjectName: 'Your project repo link',
       },
       */
   
     {
-      ContributorName: 'Krishna Mundada',
-      ContributorURL: 'https://github.com/krishna050702',
-      Project_Repo: 'https://www.youtube.com/watch?v=Tt0z8QzfiKg&ab_channel=DisneyPlusHotstar',
+      ContributorName: 'Yash Damdu',
+      ContributorURL: 'https://github.com/damduyr',
+      Project_Repo: 'https://github.com/damduyr/Demo_Unity',
     },
     
   ]

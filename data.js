6922d9b3ba83dd6454e009281cec2388acd1fa4f
rudra141 <111697664+rudra141@users.noreply.github.com>
2023-05-04 16:54:34 +0530

    Update data.js

--- data.js
+++ data.js
@@ -19,6 +19,10 @@ let unityData = [
       ContributorName: 'Lucifer MorningStar',
       ContributorURL: 'https://github.com/Lucifer0507',
       Project_Repo: 'https://www.youtube.com/watch?v=Tt0z8QzfiKg&ab_channel=DisneyPlusHotstar',
+    },
+    {
+      ContributorName: 'Rudra Ranjan',
+      ContributorURL: 'https://github.com/rudra141',
+      Project_Repo: 'https://www.youtube.com/watch?v=Tt0z8QzfiKg&ab_channel=DisneyPlusHotstar',
     }
-    
   ]

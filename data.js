8389110314d709f384f584882820e4a8bdd429c0
vighneshGupta <132568818+vighneshGupta@users.noreply.github.com>
2023-05-04 11:26:25 +0000

    vighnesh

--- data.js
+++ data.js
@@ -19,6 +19,12 @@ let unityData = [
       ContributorName: 'Lucifer MorningStar',
       ContributorURL: 'https://github.com/Lucifer0507',
       Project_Repo: 'https://www.youtube.com/watch?v=Tt0z8QzfiKg&ab_channel=DisneyPlusHotstar',
+    },
+
+    {
+      ContributorName: 'Vighnesh Gupta',
+      ContributorURL: 'https://github.com/Lucifer0507',
+      Project_Repo: 'https://github.com/vighneshGupta',
     }
-    
+
   ]

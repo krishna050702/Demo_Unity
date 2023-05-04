67b7f0922cd58c6ad772ca5dd0438c738ab62431
Akhilesh Bonde <110801340+akhileshh-b@users.noreply.github.com>
2023-05-04 16:57:26 +0530

    Updated data.js

--- data.js
+++ data.js
@@ -19,6 +19,10 @@ let unityData = [
       ContributorName: 'Lucifer MorningStar',
       ContributorURL: 'https://github.com/Lucifer0507',
       Project_Repo: 'https://www.youtube.com/watch?v=Tt0z8QzfiKg&ab_channel=DisneyPlusHotstar',
-    }
-    
+    },
+    {
+          ContributorName: 'Akhileshh',
+          ContributorURL: "https://github.com/akhileshh-b",
+          ProjectName: 'https://github.com/akhileshh-b/Unity1',
+      },
   ]

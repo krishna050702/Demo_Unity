1377a13b3d657d26cb8574d04fa87de498f39bff
Kartik Mahadev Vhonale <98228313+KartikVhonale@users.noreply.github.com>
2023-05-04 11:26:59 +0000

    edited

--- data.js
+++ data.js
@@ -14,7 +14,11 @@ let unityData = [
       ContributorURL: 'https://github.com/krishna050702',
       Project_Repo: 'https://www.youtube.com/watch?v=Tt0z8QzfiKg&ab_channel=DisneyPlusHotstar',
     },
-
+    {
+    ContributorName: 'Lucifer MorningStar',
+    ContributorURL: 'https://github.com/Lucifer0507',
+    Project_Repo: 'https://www.youtube.com/watch?v=Tt0z8QzfiKg&ab_channel=DisneyPlusHotstar',
+    },
     {
       ContributorName: 'Karitk Vhonale',
       ContributorURL: 'https://github.com/KartikVhonale',
